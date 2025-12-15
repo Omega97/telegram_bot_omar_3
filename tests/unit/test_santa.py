91e51ae943aaeb1365be8406b91d5d1836fc1d55
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2025-12-15 10:33:18 +0100

    Update test_santa.py

--- tests/unit/test_santa.py
+++ tests/unit/test_santa.py
@@ -1,17 +1,35 @@
+"""
+This test validates the correctness and validity of the RNG.
+"""
 from omar_bot.services.santa_v2 import SantaService
 from omar_bot.config.settings import USERS_DIR
 from omar_bot.services.user_service import UserService
 
 
-def test_1():
+_CORRECT_PAIRINGS = {
+    231576312: 130266190,
+    197127934: 231576312,
+    473531951: 197127934,
+    156267213: 473531951,
+    890008145: 156267213,
+    29421735: 890008145,
+    813074514: 29421735,
+    213607266: 813074514,
+    130266190: 213607266,
+}
+
+
+def test_1(year=2025):
+    # Generate pairings
     user_service = UserService(users_dir=USERS_DIR)
     santa_service = SantaService(user_service, group_name="santa")
-    pairings = santa_service.get_pairings()
+    pairings = santa_service.get_pairings(year=year)
 
-    for gifter_id, correct_giftee_id in pairings.items():
+    # Compare to the table
+    for gifter_id, giftee_id in pairings.items():
         gifter_name = santa_service.get_user_name(gifter_id)
-        giftee_name = santa_service.get_user_name(correct_giftee_id)
-        print(f"{gifter_name} -> {giftee_name}")
+        giftee_name = santa_service.get_user_name(giftee_id)
+        assert _CORRECT_PAIRINGS[gifter_id] == giftee_id
 
 
 if __name__ == '__main__':
